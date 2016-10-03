c68c4fc627da8f7bf5c4fa33ad3391f38dda522f
XanderPSON <xwpeterson@gmail.com>
2016-10-02 18:31:22 -0700

    wip

--- app.js
+++ app.js
@@ -1,41 +1,62 @@
 $document.ready(){
   var refs = document.getElementsByClassName('discussion-item-ref');
-
-  var pullRequests = getPRs(refs);
+  var pullRequests = getPullRequests(refs);
   var completedDiv = buildDiv(pullRequests);
 
-  var sidebar = document.getElementById('partial-discussion-sidebar')
   var child = document.createElement('div');
-  child.innerHTML = completedDiv
+  child.innerHTML = completedDiv;
+  
+  var sidebar = document.getElementById('partial-discussion-sidebar');
   sidebar.insertBefore(child, sidebar.firstChild);
 }
 
-function getPRs(refs) {
-  // var refTitles = refs.getElementsByClassName('discussion-item-ref-title');
-  // var refStates = refs.getElementsByClassName('state');
-  var pullRequests = [];
+function getPullRequests(refs) {
+  var titles = [];
+  var states = [];
 
   for (var i = 0; i < refs.length; i++) {
-    var pullRequest = '<div>'
-    if (refs[i].getElementsByClassName('discussion-item-ref-title').length !== 0) {
-      pullRequest += refs[i].getElementsByClassName('discussion-item-ref-title')[0].outerHTML
+    var refTitles = refs[i].getElementsByClassName('discussion-item-ref-title');
+    var refStates = refs[i].getElementsByClassName('state');
+
+    for (var j = 0; j < refTitles.length; j++) {
+      titles.push(refs[i].getElementsByClassName('discussion-item-ref-title')[0].outerHTML) 
     }
-    if (refs[i].getElementsByClassName('state').length !== 0) {
-      pullRequest += refs[i].getElementsByClassName('state')[0].outerHTML
+
+    for (var k = 0; k < refTitles.length; k++) {
+      states.push(refs[i].getElementsByClassName('state')[0].outerHTML)
     }
-    pullRequest += '</div>'
-    pullRequests.push(pullRequest);
   }
 
-  return pullRequests;
+  return [titles, states];
 }
 
 function buildDiv(pullRequests) {
-  var myDiv = '<div>'
-  for (var i = 0; i < pullRequests.length; i++){
-    myDiv += pullRequests[i];
+  var myDiv = buildFirstHalf();
+  for (var i = 0; i < pullRequests[0].length; i++){
+    myDiv += pullRequests[0][i];
+    myDiv += pullRequests[1][i];
   }
-  myDiv += '</div>';
+  myDiv += '</div></div></div>';
 
   return myDiv;
 }
+
+function buildFirstHalf() {
+  var myDiv = ''
+  myDiv += '<div class="discussion-sidebar-item sidebar-pull-requests js-discussion-sidebar-item">'
+  myDiv += '<div class="select-menu js-menu-container js-select-menu" data-contents-url="">'
+  myDiv += '<button type="button" class="discussion-sidebar-heading discussion-sidebar-toggle js-menu-target" aria-label="View pull requests" aria-haspopup="true" data-hotkey="m" aria-expanded="false">'
+  myDiv += 'Pull Requests'
+  myDiv += '</button>'
+  myDiv += '<div class="pull-requests css-truncate">'
+  return myDiv;
+}
+
+function buildSecondHalf(myDiv) {
+
+  return myDiv;
+}
+
+  myDiv += '<a title="Pull requests" href="/Roostify/team-platform/milestone/20" class="milestone-name css-truncate">'
+  myDiv += '<span class="css-truncate-target">Pull requestsasdsadas</span>'
+  myDiv += '</a>'
